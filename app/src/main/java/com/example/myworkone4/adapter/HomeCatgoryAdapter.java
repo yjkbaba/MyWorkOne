f9a705ccdb54b7609993d8cdec59e6d4e7a318c4
杨继科 <1206795862@qq.com>
2020-06-23 11:21:29 +0800

    yjk

--- app/src/main/java/com/example/myworkone4/adapter/HomeCatgoryAdapter.java
+++ app/src/main/java/com/example/myworkone4/adapter/HomeCatgoryAdapter.java
@@ -1,35 +1,28 @@
 package com.example.myworkone4.adapter;
-
-
-//import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.myworkone4.bean.HomeCategory;
-//import android.support.v7.widget.RecyclerView;
+import com.example.myworkone4.bean.HomeCampaign;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.myworkone4.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
-//import android.support.v7.widget.RecyclerView;
 public class HomeCatgoryAdapter extends RecyclerView.Adapter<HomeCatgoryAdapter.ViewHolder>{
 
 
     private  static int VIEW_TYPE_L=0;
     private  static int VIEW_TYPE_R=1;
-
     private LayoutInflater mInflater;
+    private List<HomeCampaign> mDatas;
+    private Context mContext;
 
-
-
-    private List<HomeCategory> mDatas;
-
-    public HomeCatgoryAdapter(List<HomeCategory> datas){
+    public HomeCatgoryAdapter(List<HomeCampaign> datas, Context context){
         mDatas = datas;
+        this.mContext = context;
     }
 
         @Override
@@ -37,52 +30,37 @@ public class HomeCatgoryAdapter extends RecyclerView.Adapter<HomeCatgoryAdapter.
 
             mInflater = LayoutInflater.from(viewGroup.getContext());
             if(type == VIEW_TYPE_R){
-
                 return  new ViewHolder(mInflater.inflate(R.layout.template_home_cardview2,viewGroup,false));
             }
-
             return  new ViewHolder(mInflater.inflate(R.layout.template_home_cardview,viewGroup,false));
-
-
         }
 
         @Override
         public void onBindViewHolder(ViewHolder viewHolder, int i) {
 
-            HomeCategory category = mDatas.get(i);
-            viewHolder.textTitle.setText(category.getName());
-            viewHolder.imageViewBig.setImageResource(category.getImgBig());
-            viewHolder.imageViewSmallTop.setImageResource(category.getImgSmallTop());
-            viewHolder.imageViewSmallBottom.setImageResource(category.getImgSmallBottom());
-
-
+            HomeCampaign homeCampaign = mDatas.get(i);
+            viewHolder.textTitle.setText(homeCampaign.getTitle());
+            Picasso.with(mContext).load(homeCampaign.getCpOne().getImgUrl()).into(viewHolder.imageViewBig);
+            Picasso.with(mContext).load(homeCampaign.getCpTwo().getImgUrl()).into(viewHolder.imageViewSmallTop);
+            Picasso.with(mContext).load(homeCampaign.getCpThree().getImgUrl()).into(viewHolder.imageViewSmallBottom);
         }
-
         @Override
         public int getItemCount() {
             return mDatas.size();
         }
-
-
         @Override
         public int getItemViewType(int position) {
-
             if(position % 2==0){
                 return  VIEW_TYPE_R;
             }
             else return VIEW_TYPE_L;
-
-
         }
 
         static  class ViewHolder extends RecyclerView.ViewHolder{
-
-
             TextView textTitle;
             ImageView imageViewBig;
             ImageView imageViewSmallTop;
             ImageView imageViewSmallBottom;
-
             public ViewHolder(View itemView) {
                 super(itemView);
                 textTitle = (TextView) itemView.findViewById(R.id.text_title);
@@ -90,6 +68,5 @@ public class HomeCatgoryAdapter extends RecyclerView.Adapter<HomeCatgoryAdapter.
                 imageViewSmallTop = (ImageView) itemView.findViewById(R.id.imgview_small_top);
                 imageViewSmallBottom = (ImageView) itemView.findViewById(R.id.imgview_small_bottom);
             }
-
         }
 }
--- app/src/main/java/com/example/myworkone4/fragment/HomeFragment.java
+++ app/src/main/java/com/example/myworkone4/fragment/HomeFragment.java
@@ -9,32 +9,23 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
-import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.example.myworkone4.Contants;
 import com.example.myworkone4.R;
 import com.example.myworkone4.adapter.DividerItemDecortion;
 import com.example.myworkone4.bean.Banner;
-import com.example.myworkone4.bean.HomeCategory;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
+import com.example.myworkone4.bean.HomeCampaign;
 import java.util.List;
 import com.example.myworkone4.adapter.HomeCatgoryAdapter;
+import com.example.myworkone4.http.BaseCallback;
+import com.example.myworkone4.http.OkHttpHelper;
+import com.example.myworkone4.http.SpotsCallBack;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import org.jetbrains.annotations.NotNull;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
-
-import static android.content.ContentValues.TAG;
 
 
 /**
@@ -43,119 +34,81 @@ import static android.content.ContentValues.TAG;
 public class HomeFragment extends Fragment {
 
     private SliderLayout mSliderLayout;
-    private PagerIndicator indicator;
     private RecyclerView mRecyclerView;
     private HomeCatgoryAdapter mAdatper;
     private static  final  String TAG="HomeFragment";
     private Gson mGson = new Gson();
     private List<Banner> mBanner;
+    private OkHttpHelper httpHelper = OkHttpHelper.getInstance();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.fragment_home,container,false);
         mSliderLayout= view.findViewById(R.id.slider);
-        indicator=view.findViewById(R.id.custom_indicator);
         requestImages();
-
-
         initRecyclerView(view);
-
         return view;
     }
-
     private void requestImages(){
-        String url ="http://112.124.22.238:8081/course_api/banner/query";
-        OkHttpClient client = new OkHttpClient();
-        RequestBody body = new FormBody.Builder()
-                .add("type","1")
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
-        try {
-            client.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+        String url ="http://112.124.22.238:8081/course_api/banner/query?type=1";
+        httpHelper.get(url, new SpotsCallBack<List<Banner>>(getContext()){
+            @Override
+            public void onSuccess(Response response, List<Banner> banners) {
+                mBanner = banners;
+                initSlider();
+            }
 
-                }
+            @Override
+            public void onError(Response response, int code, Exception e) {
 
-                @Override
-                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                    if (response.isSuccessful()) {
-                        String json;
-                        json = response.body().string();
-                        Type type = new TypeToken<List<Banner>>(){}.getType();
-                        mBanner = mGson.fromJson(json,type);
-                        initSlider();
-                    }
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            }
+        });
     }
 
     private void initRecyclerView(View view) {
-
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+        httpHelper.get(Contants.API.CAMPAIGN_HOME, new BaseCallback<List<HomeCampaign>>() {
+            public void onBeforeRequest(Request request) {
 
-        List<HomeCategory> datas = new ArrayList<>(15);
+            }
+            public void onFailure(Request request, Exception e) {
 
-        HomeCategory category = new HomeCategory("热门活动",R.drawable.img_big_1,R.drawable.img_1_small1,R.drawable.img_1_small2);
-        datas.add(category);
+            }
+            public void onResponse(Response response) {
 
-        category = new HomeCategory("有利可图",R.drawable.img_big_4,R.drawable.img_4_small1,R.drawable.img_4_small2);
-        datas.add(category);
-        category = new HomeCategory("品牌街",R.drawable.img_big_2,R.drawable.img_2_small1,R.drawable.img_2_small2);
-        datas.add(category);
+            }
+            public void onSuccess(Response response, List<HomeCampaign> homeCampaigns) {
 
-        category = new HomeCategory("金融街 包赚翻",R.drawable.img_big_1,R.drawable.img_3_small1,R.drawable.imag_3_small2);
-        datas.add(category);
+                initData(homeCampaigns);
+            }
+            public void onError(Response response, int code, Exception e) {
 
-        category = new HomeCategory("超值购",R.drawable.img_big_0,R.drawable.img_0_small1,R.drawable.img_0_small2);
-        datas.add(category);
+            }
+        });
+    }
 
-        mAdatper = new HomeCatgoryAdapter(datas);
-
+    private  void initData(List<HomeCampaign> homeCampaigns){
+        mAdatper = new HomeCatgoryAdapter(homeCampaigns,getActivity());
         mRecyclerView.setAdapter(mAdatper);
         mRecyclerView.addItemDecoration(new DividerItemDecortion());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-
     }
 
     private void initSlider(){
-
         if(mBanner !=null){
-
             for (Banner banner : mBanner){
                 TextSliderView textSliderView = new TextSliderView(this.getActivity());
                 textSliderView.image(banner.getImgUrl());
                 textSliderView.description(banner.getName());
                 textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
                 mSliderLayout.addSlider(textSliderView);
-
             }
         }
-
-
-
-
-
-
-
-
-        //mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);//底部中间位置
-        mSliderLayout.setCustomIndicator(indicator);
+        mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);//底部中间位置
         mSliderLayout.setCustomAnimation(new DescriptionAnimation());
-        mSliderLayout.setPresetTransformer(SliderLayout.Transformer.Stack);//设置轮播样式
+        mSliderLayout.setPresetTransformer(SliderLayout.Transformer.RotateUp);//设置轮播样式
         mSliderLayout.setDuration(3000);//设置时间
-
-
-
-
-
         mSliderLayout.addOnPageChangeListener(new ViewPagerEx.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -170,14 +123,11 @@ public class HomeFragment extends Fragment {
                 Log.d(TAG,"onPageScrollStateChanged");
             }
         });
-
-
-
-
-
     }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
 
-
-
-
+        mSliderLayout.stopAutoCycle();
+    }
 }
